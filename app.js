8edcbb5863bcfceaf6ffd898f774865eed145d75
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-03-21 15:17:10 +0200

    add routes

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const http = require("http");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-
-  res.write("<html>");
-  res.write("<head><title>Response</title></head>");
-  res.write("<body><h1>Server is running...</h1></body>");
-  res.write("</head>");
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(4500);
