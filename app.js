f79a725c6e35b37fec167a70fc462f63f541441f
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-03-22 18:44:37 +0200

    expand routes

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use("/", (req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 
 app.listen(4500);
--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,11 @@ const router = Router();
 
 router.get("/add-user", (req, res, next) => {
   res.send(
-    "<form action='/user' method='POST'><input type='text' name='name' /><button type='submit'>Add User</button></form>"
+    "<form action='/admin/add-user' method='POST'><input type='text' name='name' /><button type='submit'>Add User</button></form>"
   );
 });
 
-router.post("/user", (req, res, next) => {
+router.post("/add-user", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
