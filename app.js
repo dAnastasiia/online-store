d4b21be52a22f6584cc5c0b1b58b4bdbcf1a6c42
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-03-22 16:44:19 +0200

    install express.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(4500);
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Users list</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Main Page</h1>");
+});
+
+app.listen(4500);
