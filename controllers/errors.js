02a025fbbe0c57fa62b4b1d6147f1d0682edc487
Anastasiia Drozd <da4798500@gmail.com>
2024-03-23 16:46:00 +0200

    App enhancing (#6)
    - add navigation
    - register routes
    - update controllers
    - expand product model
    - enhance styles
    - add new routes

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,5 +1,3 @@
-exports.get404Page = (req, res, next) => {
-  res
-    .status(404)
-    .render("404", { pageTitle: "Page Not Found", path: "not-found" });
+exports.get404 = (req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const router = Router();
 
-router.get("/add-product", productsController.getAddProduct);
-router.post("/add-product", productsController.postAddProduct);
+router.get("/add-product", adminController.getAddProduct);
+router.post("/add-product", adminController.postAddProduct);
+
+router.get("/products", adminController.getProducts);
 
 module.exports = router;
