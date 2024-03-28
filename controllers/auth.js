bfd9a2660e92e5f590d49fb1c5560d75ee7117b1
Anastasiia Drozd <da4798500@gmail.com>
2024-03-28 21:55:52 +0200

    Add reset password functionality (#18)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
+const crypro = require("node:crypto");
+
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const User = require("../models/user");
 
-const { DEFAULT_CART } = require("../utils/constants");
+const { DEFAULT_CART, MILLISECONDS_IN_HOUR } = require("../utils/constants");
 const EmailService = require("../services/email-sender");
 
 // ------ Signup ------
@@ -43,14 +45,14 @@ exports.postSignup = async (req, res, next) => {
       cart: DEFAULT_CART,
     });
 
-    await EmailService.sendMail({
+    await user.save();
+
+    EmailService.sendMail({
       from: "mailtrap@demomailtrap.com",
       to: email,
-      subject: "Hello from Mailtrap!",
+      subject: "You're just signed in",
       html: `<h1>${name}, welcome to system!</h1>`,
     });
-
-    await user.save();
     res.redirect("/login");
   } catch (error) {
     console.error(error);
@@ -107,4 +109,114 @@ exports.postLogout = (req, res, next) => {
     console.error(error);
     res.redirect("/");
   });
+};
+
+// ------ Reset password ------
+exports.getReset = (req, res, next) => {
+  let errorMessage = req.flash("error");
+
+  if (errorMessage.length > 0) {
+    errorMessage = errorMessage[0];
+  } else {
+    errorMessage = null;
+  }
+
+  res.render("auth/reset", {
+    pageTitle: "Reset Pssword",
+    path: "/reset-password",
+    errorMessage,
+  });
+};
+
+exports.postReset = async (req, res, next) => {
+  const { email } = req.body;
+
+  try {
+    const token = crypro.randomBytes(32).toString("hex");
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      req.flash("error", "No account with such an email");
+      return await res.redirect("/reset-password");
+    }
+
+    user.resetToken = token;
+    user.resetTokenExp = Date.now() + MILLISECONDS_IN_HOUR;
+
+    await user.save();
+
+    EmailService.sendMail({
+      from: "mailtrap@demomailtrap.com",
+      to: email,
+      subject: "Reset password",
+      html: `
+       <p>You requested password reset.</p>
+       <p>Follow <a href="http://localhost:${process.env.PORT}/set-password/${token}" target="_blank">this link</a> to set the new password.</p>
+       `,
+    });
+
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.getPassword = async (req, res, next) => {
+  const { token: resetToken } = req.params; // get url parameter
+
+  let errorMessage = req.flash("error");
+
+  if (errorMessage.length > 0) {
+    errorMessage = errorMessage[0];
+  } else {
+    errorMessage = null;
+  }
+
+  try {
+    const user = await User.findOne({
+      resetToken,
+      resetTokenExp: { $gt: Date.now() }, // $gt — GREATER THAN special comparison syntax
+    });
+
+    if (!user) return res.redirect("/");
+
+    res.render("auth/set-password", {
+      pageTitle: "Set Password",
+      path: "/set-password",
+      errorMessage,
+      userId: user._id.toString(),
+      resetToken,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.postPassword = async (req, res, next) => {
+  const { password, userId: _id, resetToken } = req.body;
+
+  try {
+    const user = await User.findOne({
+      _id,
+      resetToken,
+      resetTokenExp: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      req.flash("error", "Link has been expired");
+      return await res.redirect("/reset-password");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    user.password = hashedPassword;
+    user.resetToken = null;
+    user.resetTokenExp = null;
+
+    await user.save();
+
+    return res.redirect("/login");
+  } catch (error) {
+    console.error(error);
+  }
 };
