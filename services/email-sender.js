0a6f8d4661c18e3fef116970b0fc73f5af335e1c
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-03-28 20:46:45 +0200

    move emails to live mode

--- services/email-sender.js
+++ services/email-sender.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 
 module.exports = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
+  host: "live.smtp.mailtrap.io",
+  port: 587,
   auth: {
     user: process.env.MAILTRAP_USER,
     pass: process.env.MAILTRAP_PASSWORD,
